9bc7d74c5a9d837785b898adc46f272760b8fcb5
Edsonsantos0840 <129888422+Edsonsantos0840@users.noreply.github.com>
2023-10-07 18:02:59 -0300

    iniciando o projeto

---
+++ src/app/header/page.js
@@ -0,0 +1,15 @@
+'use client'
+//css
+import styles from '@/app/style/header.module.css'
+//módulo
+import {Link} from 'next/link'
+
+export default function Header() {
+
+  return (
+ <>
+
+ </>
+  )
+}
+
